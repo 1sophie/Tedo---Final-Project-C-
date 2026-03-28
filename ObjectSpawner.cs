8a41fb7a8de610f837ac9c3cf852133e1c734da7
SophieOne <52621399+1sophie@users.noreply.github.com>
2026-03-28 18:20:55 +0400

    Update ObjectSpawner.cs

--- ObjectSpawner.cs
+++ ObjectSpawner.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    // Drag your "Plank" Prefab into this box in the Inspector
+ 
     public GameObject plankPrefab;
 
-    // How many seconds between each drop
+  
     public float spawnRate = 2f;
 
-    // How wide is your screen? (Standard is about -8 to 8)
+  
     public float screenWidth = 8f;
 
     void Start()
     {
-        // This tells Unity: "Run the 'Spawn' function every 'spawnRate' seconds"
+    
         InvokeRepeating("Spawn", 1.0f, spawnRate);
     }
 
     void Spawn()
     {
-        // Pick a random X position
+     
         float randomX = Random.Range(-screenWidth, screenWidth);
 
         // Create the position vector (using the Spawner's height)
